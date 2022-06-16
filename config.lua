873970ab63a334dc1fedec0a1f7be92ed36952b3
Abel Gaming <lcpdpolice4939@gmail.com>
2022-06-16 06:54:22 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,4 +4,4 @@ Config.CasinoTokenItem = 'casino_token'
 Config.Position = vector3(1116.053, 219.956, -49.4425)
 Config.TokenPurchasePrice = 100
 Config.TokenSellPrice = 100
-Config.UseSWTNotifications = true
+Config.UseSWTNotifications = true --Must implement your own if set to false
